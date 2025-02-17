5fc42fbe75a70bc2a8416cf1b8db43042f9b9923
hjkwon <sirru97@naver.com>
2025-02-17 16:55:41 +0900

    test husky2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,44 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+// import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+// ];
+
+// export default eslintConfig;
+
+import next from 'next';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  next(),
+  {
+    languageOptions: {
+      parser: tsParser,
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': ['error'],
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  prettier,
 ];
 
 export default eslintConfig;
