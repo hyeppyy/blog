ba64f9b25b5e00fb2bda8dcddc4475624c18b9d4
hjkwon <sirru97@naver.com>
2025-02-17 17:40:16 +0900

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,44 +1,115 @@
-// import { dirname } from 'path';
-// import { fileURLToPath } from 'url';
-// import { FlatCompat } from '@eslint/eslintrc';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [
-//   ...compat.extends('next/core-web-vitals', 'next/typescript'),
-// ];
-
-// export default eslintConfig;
-
-import next from 'next';
+import nextPlugin from '@next/eslint-plugin-next';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
-import prettier from 'eslint-config-prettier';
-import pluginPrettier from 'eslint-plugin-prettier';
+import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import importPlugin from 'eslint-plugin-import';
 
-const eslintConfig = [
-  next(),
+/** @type {import('eslint').Linter.FlatConfig[]} */
+const config = [
   {
-    languageOptions: {
-      parser: tsParser,
-      sourceType: 'module',
-    },
     plugins: {
       '@typescript-eslint': tseslint,
-      prettier: pluginPrettier,
+      '@next/next': nextPlugin,
+      prettier: prettier,
+      react: react,
+      'react-hooks': reactHooks,
+      import: importPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
     rules: {
-      'prettier/prettier': 'error',
-      '@typescript-eslint/no-unused-vars': ['error'],
+      'prettier/prettier': ['error', { singleQuote: true }],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+        },
+      ],
       'react/react-in-jsx-scope': 'off',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      '@next/next/no-html-link-for-pages': 'error',
+      'react/function-component-definition': [
+        2,
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+      'arrow-body-style': ['error', 'as-needed'],
+      'react/jsx-pascal-case': 'error',
+      camelcase: ['error', { properties: 'never' }],
+      'react/jsx-handler-names': [
+        'error',
+        {
+          eventHandlerPropPrefix: 'handle',
+        },
+      ],
+      'import/no-default-export': 'off',
+      'import/prefer-default-export': 'off',
+      'import/no-unresolved': 'off',
+      'react-hooks/rules-of-hooks': 'error', // react-hooks 규칙 추가
+      'react-hooks/exhaustive-deps': 'warn', // react-hooks 규칙 추가
+      'react/prop-types': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'no-unused-vars': 'off',
+      eqeqeq: 'error',
+      'prefer-destructuring': [
+        'error',
+        {
+          array: false,
+          object: true,
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'index'],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          'newlines-between': 'never',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'react/jsx-no-duplicate-props': ['error', { ignoreCase: true }],
+      '@typescript-eslint/no-empty-interface': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'no-useless-rename': 'error',
+      'object-shorthand': 'error',
+      'react/jsx-key': ['error', { checkFragmentShorthand: true }],
     },
   },
-  prettier,
 ];
 
-export default eslintConfig;
+export default config;
