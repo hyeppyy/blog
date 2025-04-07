f0e9b31307da399e58e5cb41155927bc6eead9f0
hjkwon <sirru97@naver.com>
2025-04-07 23:03:03 +0900

    fix: 사이트맵 우선순위 수정 #60

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   output: 'standalone',
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
 };
 
 export default nextConfig;
